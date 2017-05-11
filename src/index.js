ded1dd4d9598afeca1110f605d4d251288c7ae70
Jean-Michel FRANCOIS <jmfrancois@talend.com>
2017-05-11 18:26:35 +0200

    fix: make nested key working

--- src/index.js
+++ src/index.js
@@ -6,8 +6,18 @@ export default (engine, whitelist = []) => {
 
         load() {
             return engine.load().then((result) => {
-                whitelist.forEach((key) => {
-                    result[key] = fromJS(result[key]);
+                whitelist.forEach((keys) => {
+                    if (typeof keys === 'string') {
+                        keys = [keys]; // eslint-disable-line no-param-reassign
+                    }
+                    let tmp = result;
+                    keys.forEach((key, index) => {
+                        if (index === keys.length - 1) {
+                            tmp[key] = fromJS(tmp[key]);
+                        } else {
+                            tmp = tmp[key];
+                        }
+                    });
                 });
                 return result;
             });
